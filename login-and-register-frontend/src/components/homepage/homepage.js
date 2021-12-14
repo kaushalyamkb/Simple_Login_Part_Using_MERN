12c5906dbc3be4c8947e8f55ac871df0efb4c8c1
Kaushalya M.K.B <86149391+IT20118372@users.noreply.github.com>
2021-12-14 08:59:41 +0530

    Update homepage.js

--- login-and-register-frontend/src/components/homepage/homepage.js
+++ login-and-register-frontend/src/components/homepage/homepage.js
@@ -4,7 +4,7 @@ import "./homepage.css"
 const Homepage = ({setLoginUser}) => {
     return (
         <div className="homepage">
-            <h1>Hello Homepage</h1>
+            <h1>This is Homepage</h1>
             <div className="button" onClick={() => setLoginUser({})} >Logout</div>
         </div>
     )
